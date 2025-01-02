4a3dd8b43bbc643e4beeea5c5212bd268f998f48
David Sergeev <prosian25@gmail/com>
2025-01-02 18:25:52 +0200

    create jwt authentication to the web app

--- src/controllers/users_controller.ts
+++
@@ -1,6 +0,0 @@
-import usersModel, { IUser } from "../models/users_model";
-import BaseController from "./base_controller";
-
-const usersController = new BaseController<IUser>(usersModel);
-
-export default usersController
